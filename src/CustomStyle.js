6e30db6436ead70a35fe9407f5dd9d4edb1a6ba3
Dodecane <20498113+Dodecane@users.noreply.github.com>
2021-04-02 22:59:55 +0800

    first commit

--- src/CustomStyle.js
+++ src/CustomStyle.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useMemo, useRef } from 'react';
-import { useThree } from 'react-three-fiber';
-import MersenneTwist from 'mersenne-twister';
-import { TorusKnot } from '@react-three/drei';
-import Color from 'color';
+import React, { useEffect, useMemo, useRef, Suspense } from "react";
+import { applyProps, useThree } from "react-three-fiber";
+import MersenneTwist from "mersenne-twister";
+import { useGLTF } from "@react-three/drei/useGLTF";
+import { toBN, fromWei } from "web3-utils";
 
 /*
 Create your Custom style to be turned into a EthBlock.art BlockStyle NFT
@@ -30,16 +30,15 @@ Getting started:
 
 // Required style metadata
 const styleMetadata = {
-  name: '',
-  description: '',
-  image: '',
-  creator_name: '',
+  name: "",
+  description: "",
+  image: "",
+  creator_name: "",
   options: {
-    mod1: 0.4,
-    mod2: 0.1,
-    mod3: 0.4,
-    color1: '#fff000',
-    background: '#000000',
+    mod1: 1,
+    mod2: 1,
+    color1: "#ffffff",
+    background: "#ffffff",
   },
 };
 
@@ -49,11 +48,10 @@ export default function CustomStyle({
   block,
   attributesRef,
 
-  mod1 = 0.75, // Example: replace any number in the code with mod1, mod2, or color values
-  mod2 = 0.25,
-  mod3 = 0.4,
-  color1 = '#4f83f1',
-  background = '#ccc',
+  mod1 = 1, // Example: replace any number in the code with mod1, mod2, or color values
+  mod2 = 1,
+  color1 = "#ffffff",
+  background = "#ffffff",
 }) {
   console.log(`rendering`);
 
@@ -72,7 +70,7 @@ export default function CustomStyle({
 
   // Update custom attributes related to style when the modifiers change
   useEffect(() => {
-    console.log('updating attributes...');
+    console.log("updating attributes...");
     attributesRef.current = () => {
       return {
         // This is called when the final image is generated, when creator opens the Mint NFT modal.
@@ -82,19 +80,17 @@ export default function CustomStyle({
 
         attributes: [
           {
-            display_type: 'number',
-            trait_type: 'your trait here number',
-            value: hoistedValue.current, // using the hoisted value from within the draw() method, stored in the ref.
+            trait_type: "palette",
+            value: hoistedValue.current.color,
           },
-
           {
-            trait_type: 'your trait here text',
-            value: 'replace me',
+            trait_type: "shape",
+            value: hoistedValue.current.shape,
           },
         ],
       };
     };
-  }, [hoistedValue, attributesRef]);
+  }, [hoistedValue.current, attributesRef]);
 
   // Handle correct scaling of scene as canvas is resized, and when generating upscaled version.
   useEffect(() => {
@@ -106,49 +102,336 @@ export default function CustomStyle({
     camera.updateProjectionMatrix();
   }, [camera, width, height]);
 
+  // Colour palettes
+  const blue1 = [
+    "#008CD9",
+    "#009FD9",
+    "#037FBF",
+    "#003990",
+    "#4A5DA2",
+    "#015EA3",
+  ];
+  const blue1mono = [
+    "#008CD9",
+    "#009FD9",
+    "#037FBF",
+    "#003990",
+    "#4A5DA2",
+    "#015EA3",
+    "#3D413D",
+    "#07050A",
+  ];
+  const blue2 = [
+    "#003990",
+    "#4A5DA2",
+    "#015EA3",
+    "#0F60A2",
+    "#3BB3E5",
+    "#989ED0",
+  ];
+  const blue2mono = [
+    "#003990",
+    "#4A5DA2",
+    "#015EA3",
+    "#0F60A2",
+    "#3BB3E5",
+    "#989ED0",
+    "#C2C3BB",
+    "#6A6F6D",
+  ];
+  const blue3 = [
+    "#0F60A2",
+    "#3BB3E5",
+    "#989ED0",
+    "#41B5DA",
+    "#56C3EA",
+    "#BBE2F1",
+  ];
+  const blue3mono = [
+    "#0F60A2",
+    "#3BB3E5",
+    "#989ED0",
+    "#41B5DA",
+    "#56C3EA",
+    "#BBE2F1",
+    "#F0F0F4",
+    "#E2E0D3",
+  ];
+  const red1 = [
+    "#E7280E",
+    "#C12823",
+    "#A40022",
+    "#760020",
+    "#E94F09",
+    "#A51052",
+  ];
+  const red1mono = [
+    "#E7280E",
+    "#C12823",
+    "#A40022",
+    "#760020",
+    "#E94F09",
+    "#A51052",
+    "#3D413D",
+    "#07050A",
+  ];
+  const red2 = [
+    "#760020",
+    "#E94F09",
+    "#A51052",
+    "#D12A78",
+    "#EEB0BD",
+    "#FCE5EB",
+  ];
+  const red2mono = [
+    "#760020",
+    "#E94F09",
+    "#A51052",
+    "#D12A78",
+    "#EEB0BD",
+    "#FCE5EB",
+    "#C2C3BB",
+    "#6A6F6D",
+  ];
+  const red3 = [
+    "#D12A78",
+    "#EEB0BD",
+    "#FCE5EB",
+    "#E1A4B3",
+    "#F8CBC8",
+    "#FEEDE5",
+  ];
+  const red3mono = [
+    "#D12A78",
+    "#EEB0BD",
+    "#FCE5EB",
+    "#E1A4B3",
+    "#F8CBC8",
+    "#FEEDE5",
+    "#F0F0F4",
+    "#E2E0D3",
+  ];
+
   // Shuffle the random number bag when block changes
-  const [color, scale, tori] = useMemo(() => {
+  const [mainRotation, jewelConfig, colours, scale, shape] = useMemo(() => {
     console.log(`shuffling...`);
     const { hash } = block;
     const seed = parseInt(hash.slice(0, 16), 16);
     shuffleBag.current = new MersenneTwist(seed);
-    const color = Color([ran255(), ran255(), ran255()]).hex();
-    const scale = shuffleBag.current.random() / 100;
-    const tori = block.transactions.map((tx, i) => {
-      const mul = 1.5;
-      const flip = i % 2 ? -1 : 1;
-      const flip2 = i % 3 ? -1 : 1;
-      const flip3 = i % 4 ? -1 : 1;
-      return [
-        shuffleBag.current.random() * mul * flip,
-        shuffleBag.current.random() * mul * flip2,
-        shuffleBag.current.random() * mul * flip3,
-      ];
-    });
 
-    function ran255() {
-      return Math.floor(255 * shuffleBag.current.random());
+    const ethTransferred = fromWei(
+      block.transactions
+        .map((tx) => toBN(tx.value.hex))
+        .reduce((a, b) => a.add(b))
+    );
+    console.log("ETH transferred: " + ethTransferred + " ETH");
+
+    const gasPriceList = block.transactions.map((tx) => toBN(tx.gasPrice.hex));
+    const avgGasPrice = fromWei(
+      gasPriceList.reduce((a, b) => a.add(b)).div(toBN(gasPriceList.length)),
+      "Gwei"
+    );
+    console.log("Average gas price: " + avgGasPrice + " Gwei");
+
+    function getScale() {
+      if (ethTransferred > 16384) {
+        return 0.00001;
+      } else {
+        return (ethTransferred / 16384) * -0.00299 + 0.003;
+      }
     }
 
-    hoistedValue.current = 42;
-    return [color, scale, tori];
+    function randomRotation() {
+      return [
+        shuffleBag.current.random() * 2 * Math.PI,
+        shuffleBag.current.random() * 2 * Math.PI,
+        shuffleBag.current.random() * 2 * Math.PI,
+      ];
+    }
+
+    function randomPositionOnHemisphere() {
+      let a = 2 * Math.PI * shuffleBag.current.random();
+      let b = Math.acos(1 - 2 * shuffleBag.current.random());
+      let x = Math.sin(a) * Math.sin(b);
+      let y = Math.cos(a) * Math.sin(b);
+      let z = Math.abs(Math.cos(b)) * -1;
+      return [x, y, z];
+    }
+
+    function randomColoursFromPalettes(palette1, palette2, palette3) {
+      return [
+        palette1[Math.floor(shuffleBag.current.random() * palette1.length)],
+        palette2[Math.floor(shuffleBag.current.random() * palette2.length)],
+        palette3[Math.floor(shuffleBag.current.random() * palette3.length)],
+      ];
+    }
+
+    function selectColours() {
+      let scale = avgGasPrice / 200;
+      switch (true) {
+        case scale <= 0.125:
+          return ["1", randomColoursFromPalettes(blue1, blue1, blue1mono)];
+        case scale <= 0.25:
+          return ["2", randomColoursFromPalettes(blue2, blue2, blue2mono)];
+        case scale <= 0.375:
+          return ["3", randomColoursFromPalettes(blue3, blue3, blue3mono)];
+        case scale <= 0.5:
+          return [
+            "4",
+            randomColoursFromPalettes(blue3mono, red2mono, blue1mono),
+          ];
+        case scale <= 0.625:
+          return [
+            "5",
+            randomColoursFromPalettes(red3mono, blue2mono, red1mono),
+          ];
+        case scale <= 0.75:
+          return ["6", randomColoursFromPalettes(red3, red3, red3mono)];
+        case scale <= 0.875:
+          return ["7", randomColoursFromPalettes(red2, red2, red2mono)];
+        default:
+          return ["8", randomColoursFromPalettes(red1, red1, red1mono)];
+      }
+    }
+
+    function getShape() {
+      let blockNumber = block.number;
+      switch (true) {
+        case blockNumber % 30 == 0:
+          return "pentagonal delta orb";
+        case blockNumber % 12 == 0:
+          return "square jewel";
+        case blockNumber % 3 == 0:
+          return "Toshie's jewel";
+        default:
+          return "square";
+      }
+    }
+
+    const mainRotation = randomRotation();
+    let jewelConfig = [];
+    for (let i = 0; i < ethTransferred; i++) {
+      jewelConfig.push({
+        position: randomPositionOnHemisphere(),
+        rotation: randomRotation(),
+      });
+    }
+    const colourConfig = selectColours();
+    const colours = colourConfig[1];
+    const scale = getScale();
+    const shape = getShape();
+
+    const attributes = {
+      color: colourConfig[0],
+      shape: shape,
+    };
+    hoistedValue.current = attributes;
+
+    return [mainRotation, jewelConfig, colours, scale, shape];
   }, [block]);
+
+  //Insert main model
+  function Orb(props) {
+    const group = useRef();
+    const { nodes, materials } = useGLTF(props.path);
+    return (
+      <group
+        ref={group}
+        {...props}
+        dispose={null}
+        scale={[0.007, 0.007, 0.007]}
+      >
+        <mesh geometry={nodes.Body1.geometry}>
+          <meshStandardMaterial color={props.colour1} />
+        </mesh>
+        <mesh geometry={nodes.Body2.geometry}>
+          <meshStandardMaterial color={props.colour2} />
+        </mesh>
+        <mesh geometry={nodes.Body3?.geometry}>
+          <meshStandardMaterial color={props.colour3} />
+        </mesh>
+      </group>
+    );
+  }
+
+  //Insert small model
+  function Jewel(props) {
+    const group = useRef();
+    const { nodes, materials } = useGLTF(props.path);
+    return (
+      <group
+        ref={group}
+        {...props}
+        dispose={null}
+        scale={[props.scale, props.scale, props.scale]}
+      >
+        <mesh geometry={nodes.Body1.geometry}>
+          <meshPhysicalMaterial
+            transparent={true}
+            transmission={0.9}
+            color={props.colour1}
+          />
+        </mesh>
+        <mesh geometry={nodes.Body2.geometry}>
+          <meshPhysicalMaterial
+            transparent={true}
+            transmission={0.9}
+            color={props.colour2}
+          />
+        </mesh>
+        <mesh geometry={nodes.Body3?.geometry}>
+          <meshPhysicalMaterial
+            transparent={true}
+            transmission={0.9}
+            color={props.colour3}
+          />
+        </mesh>
+      </group>
+    );
+  }
+
+  //Load jewelConfig
+  function Jewels(props) {
+    const jewels = jewelConfig.map((item, index) => (
+      <Jewel
+        key={index}
+        path={props.path}
+        colour1={props.colour1}
+        colour2={props.colour2}
+        colour3={props.colour3}
+        position={item.position}
+        rotation={item.rotation}
+        scale={props.scale}
+      />
+    ));
+    return jewels;
+  }
 
   // Render the scene
   return (
-    <group ref={group} position={[-0, 0, 0]} rotation={[0, mod2, 0]}>
-      <ambientLight intensity={1} color={color} />
-      {tori.map((sp, index) => {
-        return (
-          <group key={index} position={sp}>
-            <TorusKnot
-              args={[scale * 100, mod1 / 10, (mod2 + 0.001) * 500, mod3 * 12]}
-            >
-              <meshNormalMaterial attach="material" />
-            </TorusKnot>
-          </group>
-        );
-      })}
+    <group ref={group} position={[0, 0, 0]}>
+      <directionalLight position={[0, 0, 1]} />
+      <pointLight position={[0, 0, -0.5]} color={color1} />
+      <Suspense fallback={null}>
+        <group ref={group} rotation={[0, 0, mod1 * 2 * Math.PI]}>
+          <Orb
+            path={`models/${shape}.gltf`}
+            rotation={mainRotation}
+            colour1={colours[0]}
+            colour2={colours[1]}
+            colour3={colours[2]}
+          />
+        </group>
+        <group ref={group} rotation={[0, 0, mod2 * 2 * Math.PI]}>
+          <Jewels
+            path={`models/${shape}.gltf`}
+            colour1={colours[0]}
+            colour2={colours[1]}
+            colour3={colours[2]}
+            scale={scale}
+          />
+        </group>
+      </Suspense>
     </group>
   );
 }
